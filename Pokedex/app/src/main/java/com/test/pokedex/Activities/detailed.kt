5da1825aa4f1c82df370f5944dfb21b2a4c75081
carlos <car_alf_98@icloud.com>
2020-03-21 22:45:05 -0600

    Subir funcionando todo, solo para prueba

---
+++ Pokedex/app/src/main/java/com/test/pokedex/Activities/detailed.kt
@@ -0,0 +1,146 @@
+package com.test.pokedex.Activities
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.gson.JsonObject
+import com.koushikdutta.ion.Ion
+import com.test.pokedex.R
+
+class detailed : AppCompatActivity() {
+
+    private var contexto: Context = this
+    private lateinit var data: JsonObject
+    private lateinit var linearLayoutManager: LinearLayoutManager
+
+    private var pokedex: String = "0"
+    private var aux: String =""
+    private var aux2: String = ""
+    private var aux3: String = ""
+    private lateinit var nombre: TextView
+    private lateinit var imagen: ImageView
+    private lateinit var pokemontipos : TextView
+    private lateinit var pokemonbase : TextView
+    private lateinit var pokemonmovimientos : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailed)
+        manageIntent()
+        initializeComponents()
+        initializeData()
+    }
+
+    private fun manageIntent(){
+        if(intent != null){
+            pokedex = intent.getStringExtra("Numero")
+
+        }
+    }
+
+    private fun initializeComponents(){
+        nombre = findViewById(R.id.pokemon_names)
+        imagen = findViewById(R.id.pokemon_image)
+        pokemontipos = findViewById(R.id.pokemon_types)
+        pokemonmovimientos = findViewById(R.id.pokemon_movimientos)
+        pokemonbase = findViewById(R.id.pokemon_base)
+    }
+
+    private fun initializeData(){
+
+        Ion.with(contexto)
+            .load("https://pokeapi.co/api/v2/pokemon/" + pokedex + "/")
+            .asJsonObject()
+            .done{
+                    e,result ->
+                if(e == null){
+                    data = result
+                    if(!data.get("sprites").isJsonNull){
+                        if(data.get("sprites").asJsonObject.get("front_default") != null){
+                            Glide
+                                .with(contexto)
+                                .load(data.get("sprites").asJsonObject.get("front_default").asString)
+                                .placeholder(R.drawable.pokemon_logo_min)
+                                .into(imagen)
+                        }
+                        else{
+                            imagen.setImageDrawable(
+                                ContextCompat.getDrawable( contexto,
+                                    R.drawable.pokemon_logo_min)
+                            )
+                        }
+                    }
+                    else{
+                        imagen.setImageDrawable(ContextCompat.getDrawable(contexto,
+                            R.drawable.pokemon_logo_min))
+                        Log.e("JSON","No hay Sprite")
+                    }
+
+                    if(data.get("name") != null){
+                        val completoNombre: String =
+                            "#$pokedex: " + (this.data.get("name").toString().replace(
+                                "\"",
+                                ""
+                            ))
+                        nombre.text = completoNombre
+                    }else{
+                        nombre.text="No disponible por ahora"
+                    }
+                    if(!data.get("types").isJsonNull){
+                        aux = "Tipos: \n"
+                        val tipos_aux = data.get("types").asJsonArray
+                        for (n in 0.until(tipos_aux.size())){
+                            val subtipo = tipos_aux.get(n).asJsonObject.get("type").asJsonObject
+                            val nombreTipo = subtipo.get("name").toString()
+                            aux += nombreTipo
+                            aux += "\n"
+                        }
+                        pokemontipos.text = aux
+                    }
+                    else{
+                        Log.e("Json Error", "No hay información")
+                    }
+                    if(!data.get("moves").isJsonNull){
+                        aux2 = "Movimientos: \n"
+                        val movimientos_aux = data.get("moves").asJsonArray
+                        for(n in 0.until(movimientos_aux.size())){
+                            val nombre_movimiento = movimientos_aux.get(n).asJsonObject.get("move").asJsonObject.get("name")
+                            val nombre_string = nombre_movimiento.toString()
+                            aux2+=nombre_string
+                            aux2+="\n"
+                        }
+                        pokemonmovimientos.text = aux2
+                    }
+                    else{
+                        pokemonmovimientos.text = "No Movimientos"
+                        Log.e("JsoN eRROR","No hay informaciòn disponible")
+                    }
+
+                    if(!data.get("stats").isJsonNull){
+                        aux3 = "Bases \n"
+                        val stats = data.get("stats").asJsonArray
+                        for(n in 0.until(stats.size())){
+                            val basestat = stats.get(n).asJsonObject.get("base_stat")
+                            val stat_name = stats.get(n).asJsonObject.get("stat").asJsonObject.get("name")
+                            aux3 += stat_name
+                            aux3+= "\n"
+                        }
+                        pokemonbase.text = aux3
+                    }
+                    else{
+                        Log.e("JSON", "Stats bases nulo")
+                    }
+
+                }
+            }
+
+    }
+
+
+}
--- Pokedex/app/src/main/java/com/test/pokedex/Adapters/AdapterList.kt
+++ Pokedex/app/src/main/java/com/test/pokedex/Adapters/AdapterList.kt
@@ -2,7 +2,6 @@ package com.test.pokedex.Adapters
 
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +13,7 @@ import com.bumptech.glide.Glide
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.koushikdutta.ion.Ion
-import com.test.pokedex.Activities.ActivityItem
+import com.test.pokedex.Activities.detailed
 import com.test.pokedex.R
 
 class AdapterList:RecyclerView.Adapter<AdapterList.ViewHolder>() {
@@ -45,15 +44,15 @@ class AdapterList:RecyclerView.Adapter<AdapterList.ViewHolder>() {
     class ViewHolder(view: View):RecyclerView.ViewHolder(view){
         private var imagePokemon: ImageView = view.findViewById(R.id.pokemon_image)
         private var namePokemon:TextView  = view.findViewById(R.id.pokemon_name)
-        private var imagen = view
+        private var myVista = view
 
 
         fun bind(item:JsonObject,context:Context){
 
-            imagen.tag=position
+            myVista.tag= position
             imagePokemon.setOnClickListener{
-                val posicion = imagen.tag as Int
-                var intent: Intent = Intent(context, ActivityItem::class.java)
+                val posicion = myVista.tag as Int
+                var intent: Intent = Intent(context, detailed::class.java)
                 intent.putExtra("Numero", (posicion + 1).toString())
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_SINGLE_TOP)
                 context.startActivity(intent)
